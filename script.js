9c328cf4074b9142d27dbee47d60c9085accd542
monkeymic <r3alslon@ya.ru>
2026-01-06 15:18:31 +0400

    add validation of inputs

--- script.js
+++ script.js
@@ -10,6 +10,48 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputTemp = document.querySelector(".form__input--temp");
 const inputClimb = document.querySelector(".form__input--climb");
 
+class Workout {
+
+    date = new Date();
+    id = (Date.now() + "").slice(-10);
+
+    constructor(coords, distance, duration) {
+        this.coords = coords;
+        this.distance = distance; //km
+        this.duration = duration; //min
+    }
+}
+
+class Running extends Workout {
+
+    constructor(coords, distance, duration, temp) {
+        super(coords, distance, duration);
+        this.temp = temp;
+        this.calculatePace();
+    }
+
+    calculatePace() {
+        this.pace = this.duration / this.distance;
+    }
+}
+
+class Cycling extends Workout {
+
+    constructor(coords, distance, duration, climb) {
+        super(coords, distance, duration);
+        this.climb = climb;
+        this.calculateSpeed();
+    }
+
+    calculateSpeed() {
+        this.speed = this.distance / this.duration / 60;
+    }
+}
+
+const running = new Running([60, 37], 7, 30, 25);
+const cycling = new Cycling([50, 65], 18, 33, 25);
+console.log(running, cycling);
+
 class App {
 
     #map;
@@ -66,15 +108,46 @@ class App {
     };
 
     _newWorkout(e) {
+
+        const areNumbers = function (...numbers) {
+            numbers.every(num => !Number.isFinite(num));
+        }
+
+        const areNumbersPositive = (...numbers) => {
+            numbers.every(num => num > 0);
+        }
+
         e.preventDefault();
+        //получить данные из формы
 
-        //очистка полей ввода данных
-        inputDistance.value =
-            inputDuration.value =
-            inputTemp.value =
-            inputClimb.value = "";
+        const type = inputType.value;
+        const distance = Number(inputDistance.value);
+        const duration = Number(inputDuration.value);
 
-        //отображение маркера
+        //если тренировка является пробежкой, то создать объект Running
+
+        if (type === "running") {
+            const temp = +inputTemp.value;
+            //проверить, являются ли данные валидными
+            if (!areNumbers(distance, duration, temp) || !areNumbersPositive(distance, duration, temp)) {
+                return alert("Введите положительное число");
+            }
+        }
+
+        //елси тренировка является велотренировкой, то создать объект Cycling
+
+        if (type === 'cycling') {
+            const climb = +inputClimb.value;
+            //проверить, являются ли данные валидными
+            if (!areNumbers(distance, duration, climb) || !areNumbersPositive(distance, duration)) {
+                return alert("Введите положительное число");
+            }
+        }
+
+        //добавить новый объект в массив тренировок
+
+        //отобразить тренирвку на карте
+
         const { lat, lng } = this.#mapEvent.latlng;
 
         L.marker([lat, lng]).addTo(this.#map)
@@ -86,6 +159,14 @@ class App {
                 className: 'running-popup'
             }))
             .openPopup().setPopupContent("Треня");
+
+        //отобразить тренировку в списке
+
+        //спрятать форму и очистить поля ввода данных
+        inputDistance.value =
+            inputDuration.value =
+            inputTemp.value =
+            inputClimb.value = "";
     }
 }
 
